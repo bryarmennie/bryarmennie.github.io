c7ad3b68ba8c4edfec1f7850e2fe5e51ad3c92fd
bryarmennie <bryarmennie95@gmail.com>
2016-03-01 10:41:53 +0000

    Added files via upload

---
+++ v1/javascript/javascript.js
@@ -0,0 +1,8 @@
+function menuIconClick() {
+	if (document.getElementById("menuIcon").style.display="inline" == display="inline") {
+		document.getElementById("menuIcon").Style.display="none";
+	}
+	else {
+		document.getElementById("menuIcon").style.display="inline";
+	}
+}
